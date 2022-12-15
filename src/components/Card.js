e71a51dab3939e9d5c183796064cbdfdceccaf69
Shahar Levi <shaharl@wix.com>
2022-12-15 21:31:50 +0200

    add the info

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,39 @@
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+
 export const Card = ({ character }) => {
   return (
     <div className={"card"} data-testid="card">
-      <div data-testid="cardInfoIcon">i</div>
-      <div data-testid="cardImage">
-        <img alt={character.fullName} src={character.imageUrl} />
-      </div>
-      <div data-testid="cardName">{character.fullName}</div>
+      <InfoButton character={character} />
+      <CharacterImage character={character} />
+      <CharacterName name={character.fullName} />
     </div>
   );
+};
+
+const InfoButton = ({ character }) => {
+  const { id, fullName, title, family } = character;
+  const iId = `card-info-button-${id}`;
+  const tooltipContent = `${fullName}, ${title}, House: ${family}`;
+
+  return (
+    <div className="card-info-icon-container" data-testid="cardInfoIcon">
+      <span id={iId} data-tooltip-content={tooltipContent}>
+        <strong>i</strong>
+      </span>
+      <Tooltip anchorId={iId} />
+    </div>
+  );
+};
+
+const CharacterImage = ({ character }) => {
+  return (
+    <div data-testid="cardImage">
+      <img alt={character.fullName} src={character.imageUrl} />
+    </div>
+  );
+};
+
+const CharacterName = ({ name }) => {
+  return <div data-testid="cardName">{name}</div>;
 };
